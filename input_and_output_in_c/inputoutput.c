a6b277f5e585b48aa0300871a08e4a2655113653
ManojKumarSreeram <sreerammanoj777@gmail.com>
2025-04-25 18:01:42 +0530

    added %d abd %c input logic

---
+++ input_and_output_in_c/inputoutput.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void){
+
+	/*
+	int marks;
+	printf("Enter the student marks: ");
+	scanf("%x",&marks);
+	*/
+	 
+	 
+	char grade;
+	printf("Enter the student Grade: ");
+	scanf("%c", &grade);
+	//printf("%c", &grade);
+	
+	
+	 
+	 
+	 
+	
+	
+}
